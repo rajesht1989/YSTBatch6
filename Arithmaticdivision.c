f66c74a2cda4f0f0034f91af34185932b1080b29
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 13:51:39 +0530

    Create Arithmaticdivision.c

---
+++ Arithmaticdivision.c
@@ -0,0 +1,15 @@
+//Requirement: DivisionTwonumber Arithmatic operator  and  division them print a console:
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int total;
+    printf("enter your first number");
+    scanf("%d",& a);
+     printf("enter your second number");
+    scanf("%d",& b);
+    total =a/b;
+   printf("Total is %d",total);
+    return 0;
+}
