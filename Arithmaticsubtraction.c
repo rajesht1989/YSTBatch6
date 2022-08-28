fa3f331543b8bf880fc2eff7b4be35b996ff2007
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 13:48:38 +0530

    Create Arithmaticsubtraction.c

---
+++ Arithmaticsubtraction.c
@@ -0,0 +1,15 @@
+//Requirement: subtractionTwonumber Arithmatic operator  and  subtraction them print a console:
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int total;
+    printf("enter your first number");
+    scanf("%d",& a);
+     printf("enter your second number");
+    scanf("%d",& b);
+    total =a-b;
+   printf("Total is %d",total);
+    return 0;
+}
