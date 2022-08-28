bcd4e26fcb337cc6b3953b9f7bc97ea749b19624
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 22:32:23 +0530

    Create postdecre.c

---
+++ postdecre.c
@@ -0,0 +1,11 @@
+//Requirement: number postdecrement operator  and  print a console:
+#include<stdio.h>
+int main(){
+    int a=12;
+    int b;
+b= a--;
+    printf("value of a is %d\n", b);
+    printf("value 0f b is %d\n", a);
+   return 0;
+    
+}
