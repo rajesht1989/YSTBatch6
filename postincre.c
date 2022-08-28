ead112a1f02b3de45c7067f0df070c4fa4e75006
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 22:24:50 +0530

    Create postincre.c

---
+++ postincre.c
@@ -0,0 +1,11 @@
+//Requirement: number postincrement operator  and  add them print a console:
+#include<stdio.h>
+int main(){
+    int a=12;
+    int b;
+b= a++;
+    printf("value of a is %d\n", b);
+    printf("value 0f b is %d\n", a);
+   return 0;
+    
+}
