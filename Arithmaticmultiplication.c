39c0cc57e1126838cc2a8eea81a3c61ef581e667
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 13:50:16 +0530

    Create Arithmaticmultiplication.c

---
+++ Arithmaticmultiplication.c
@@ -0,0 +1,15 @@
+//Requirement: multiplicationTwonumber Arithmatic operator  and  multiplication them print a console:
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int total;
+    printf("enter your first number");
+    scanf("%d",& a);
+     printf("enter your second number");
+    scanf("%d",& b);
+    total =a*b;
+   printf("Total is %d",total);
+    return 0;
+}
