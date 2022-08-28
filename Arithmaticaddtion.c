87f8ed7d549cb618bf1c499002feb59ed3f9df38
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 13:47:09 +0530

    Create Arithmaticaddtion.c

---
+++ Arithmaticaddtion.c
@@ -0,0 +1,15 @@
+//Requirement: addTwonumber Arithmatic operator  and  add them print a console:
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int total;
+    printf("enter your first number");
+    scanf("%d",& a);
+     printf("enter your second number");
+    scanf("%d",& b);
+    total =a+b;
+   printf("Total is %d",total);
+    return 0;
+}
