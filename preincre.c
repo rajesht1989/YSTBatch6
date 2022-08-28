8f122cafd82f52085341657f2fcd7ba685476862
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 22:23:44 +0530

    Create preincre.c

---
+++ preincre.c
@@ -0,0 +1,11 @@
+//Requirement: number preincrement operator  and  add them print a console:
+#include<stdio.h>
+int main(){
+    int a=8;
+    int b;
+b= ++a;
+    printf("value of a is %d\n", b);
+    printf("value 0f b is %d\n", a);
+   return 0;
+    
+}
