5df21e7ad3484e381d6f476ebc06d572efcd9bac
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-08-28 13:54:57 +0530

    Create Arithmaticmodules.c

---
+++ Arithmaticmodules.c
@@ -0,0 +1,15 @@
+//Requirement: ModulesTwonumber Arithmatic operator  and  modules them print a console:
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int total;
+    printf("enter your first number");
+    scanf("%d",& a);
+     printf("enter your second number");
+    scanf("%d",& b);
+    total =a%b;
+   printf("Total is %d",total);
+    return 0;
+}
