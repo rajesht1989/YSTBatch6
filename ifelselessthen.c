5e7d2e7c7c2efdc244145a48071d03c82644eb71
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-09-05 10:06:13 +0530

    Create ifelselessthen.c

---
+++ ifelselessthen.c
@@ -0,0 +1,19 @@
+// write a program get number from user and identify number is greaterthen in using if else condition
+#include<stdio.h>
+int getNumberFromUser(){
+    int number;
+    scanf("%d",&number);
+    return number;
+}
+void identifyNumber(int a){
+   if(a<100){
+       printf(" number is greater then ");
+   }else{
+        printf(" number is less then ");
+   }
+}
+void main(){
+     printf("Enter the number");
+     int a=getNumberFromUser();
+     identifyNumber(a);
+}
