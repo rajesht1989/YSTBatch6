77d66ebe58254e28a27153e8d989cb7d19a7adb5
venki-1990 <86141700+venki-1990@users.noreply.github.com>
2022-09-11 22:36:25 +0530

    Create Terinaryoperator odd even.c

---
+++ Terinaryoperator odd even.c
@@ -0,0 +1,18 @@
+//write a program to get  from user the odd or even check terinary operator on console
+ #include<stdio.h>
+ int getNumberFromUser(){
+     int number;
+     scanf("%d",&number);
+     return number;
+ }
+ void terinaryOperatorNumber(int a){
+    
+          (a %2==0) ? printf("odd") : printf("even");
+        
+        
+ }
+ void main(){
+     printf("Enter the number ");
+     int a=getNumberFromUser();
+    terinaryOperatorNumber(a);
+ }
